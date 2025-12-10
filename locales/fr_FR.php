39d37445d2ecf362499d9a71b05cb7cb19572f63
Juan Carlos Acosta Perabá <juancarlos.ap.dev@gmail.com>
2025-12-10 08:36:55 +0000

    Add French language support for task status display in locales/fr_FR.php

---
+++ locales/fr_FR.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('GLPI_ROOT')) {
+    die('Direct access not allowed');
+}
+
+$LANG['plugin_taskstatus'] = [
+    'name'        => 'Task Status Display',
+    'description' => 'Affiche visuellement le statut des tâches dans la chronologie des tickets',
+];
